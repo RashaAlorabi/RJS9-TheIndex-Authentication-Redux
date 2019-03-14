11774c03b37466183965704a322a372344f5b0d0
Rasha Alharhi <rasha.alorabi@gmail.com>
2019-03-14 20:39:09 +0300

    Authentication-Redux

---
+++ src/Logout.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as actionCreators from "./store/actions";
+const Logout = props => {
+  return (
+    <button className="btn btn-danger" onClick={props.logout}>
+      Logout {props.user.username}
+    </button>
+  );
+};
+
+const mapStateToProps = state => ({
+  user: state.auth.user
+});
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(actionCreators.logout())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Logout);
